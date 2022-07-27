cb5964e3ea5be3f5650a8edeb8b8fb321f7e9da8
Leo Prado <leonardodoprado231@gmail.com>
2022-07-27 01:47:44 -0300

    Commit functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi'
+import './styles.css'
+
+import api from './services/api'
 
 function App() {
+
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({});
+
+  async function handleSearch() {
+    // 99760000/json/
+    // 01310913/json/
+
+    if(input === ''){
+      alert("Preencha o CEP!")
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput('');
+
+    } catch {
+      alert("Ops: ERRO ao buscar CEP!");
+      setInput('');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className='title'>Buscador de CEP</h1>
+
+      <div className="container-input">
+        <input 
+        type="text" 
+        placeholder="Digite o CEP que deseja buscar..."
+        value={input}
+        onChange={(e) => setInput(e.target.value) }
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color='white' />
+        </button>
+      </div>
+
+      {Object.keys(cep).length > 1 && (
+        <main className='main'>
+          <h2>CEP: {cep.cep}</h2>
+
+          <span>{cep.rua}</span>
+          {cep.complemento !== '' && <span>Complemento: {cep.complemento}</span>}
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+        </main>
+      )};
     </div>
   );
 }
